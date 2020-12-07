5081b5343b9ceeabeb3e342878c60f6c60062724
eltoncampos1 <qazwsx272518>
2020-12-07 07:12:28 -0300

    work with promises

--- fundaments-js/02-nodejs/index.js
+++ fundaments-js/02-nodejs/index.js
@@ -1,23 +1,31 @@
+const util = require('util')
 
-function getUser(callback) {
-    setTimeout(() => {
-      return callback(null, {
-        id: 1,
-        name: "elton"
-      });
-    }, 1000);
+const getAddressAsync = util.promisify(getAddress)
+
+function getUser() {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+          return resolve({
+            id: 1,
+            name: "elton",
+            birthday: new Date()
+          });
+        }, 1000);
+    })
   }
   
-  function getTel(userId, callback) {
-    setTimeout(() => {
-      return callback(null, {
-        tel: "1234-56789",
-        ddd: 11
-      });
-    }, 2000);
+function getTel(userId, ) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+          return resolve( {
+            tel: "1234-56789",
+            ddd: 11
+          });
+        }, 2000);
+    })
   }
   
-  function getAddress(userId, callback) {
+function getAddress(userId, callback) {
     setTimeout(() => {
       return callback(null, {
         address: "Silicon valey",
@@ -25,30 +33,69 @@ function getUser(callback) {
       })
     }, 2000)
   }
-  
-  getUser(function resolveUser(error, user) {
-    if (error) {
-      console.error("Got error in user", error);
-      return;
-    }
-    getTel(user.id, function (error1, tel) {
-      if (error1) {
-        console.error("Got error in telephone", error1);
-        return;
+
+const userPromise = getUser()
+
+userPromise
+  .then((user) => {
+      return getTel(user.id)
+          .then((result) => {
+              return {
+                  user: {
+                    name: user.name,
+                    id: user.id
+                  },
+                  tell: result
+              }
+          })
+  })
+  .then((results) => {
+    const address = getAddressAsync(results.user.id)
+    return address.then((result) => {
+      return {
+        user : results.user,
+        tell: results.tell,
+        address: result,
       }
+    })
+  })
+  .then((result) => {
+    console.log(`
+      Name: ${result.user.name}
+      Adress: ${result.address.address}, ${result.address.number}
+      Tell: (${result.tell.ddd}) ${result.tell.tel}
+    `);
+  })
+  .catch((error) => {
+    console.error('get bad', error)
+  })
+
+
+
   
-      getAddress(user.id, function (error2, address) {
-        if (error2) {
-          console.error("Got error in user", error2);
-          return;
-        }
+// getUser((error, user)=> {
+//     if (error) {
+//       console.error("Got error in user", error);
+//       return;
+//     }
+//     getTel(user.id, (error1, tel) => {
+//       if (error1) {
+//         console.error("Got error in telephone", error1);
+//         return;
+//       }
+  
+//       getAddress(user.id, (error2, address)=> {
+//         if (error2) {
+//           console.error("Got error in user", error2);
+//           return;
+//         }
         
-        console.log(`
-          Name: ${user.name},
-          Tel: (${tel.ddd}) ${tel.tel},
-          Address: ${address.address}, ${address.number}
-        `)
-      });
-    });
-  });
+//         console.log(`
+//           Name: ${user.name},
+//           Tel: (${tel.ddd}) ${tel.tel},
+//           Address: ${address.address}, ${address.number}
+//         `)
+//       });
+//     });
+// });
   
