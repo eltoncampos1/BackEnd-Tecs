d33fb1993ebc129428c04f9447268d997131281c
eltoncampos1 <eltoncampos36@gmail.com>
2020-12-15 13:37:05 -0300

    refactoring mongodb

---
+++ fundaments-js/10-mult-db-refactory-mongo/src/tests/mondodbStrategy.test.js
@@ -0,0 +1,65 @@
+const assert = require('assert')
+const MongoDB = require('./../db/strategies/mongodb/mongo')
+const HeroiSchema = require('../db/strategies/mongodb/schemas/herosSchema')
+const Context = require('./../db/strategies/base/contextStrategy');
+
+const MOCK_HERO_DEFAULT = {
+    name: 'SpiderMan',
+    power: 'Super web'
+}
+const MOCK_HERO_RESGISTER = {
+    name: `WonderWoman-${Date.now()}`,
+    power: 'Tie'
+}
+
+const MOCK_HERO_UPDATE = {
+    name: `Arrow-${Date.now()}`,
+    power: 'Arrows'
+}
+
+let MOCK_HERO_ID = ''
+
+
+let context = {}
+
+describe('MongoDB tests suite', function () {
+    this.beforeAll(async () => {
+        const connection = MongoDB.connect()
+        context = new Context(new MongoDB(connection, HeroiSchema))
+        await context.create(MOCK_HERO_DEFAULT)
+        const result = await context.create(MOCK_HERO_UPDATE)
+        MOCK_HERO_ID = result._id
+    })
+    it('connection verification', async () => {
+        const result = await context.isConnected()
+        const expected = "Connected"
+
+        assert.deepStrictEqual(result, expected)
+    })
+
+    it('resgister', async () => {
+        const { name, power } = await context.create(MOCK_HERO_RESGISTER)
+        assert.deepStrictEqual({name, power}, MOCK_HERO_RESGISTER)
+    })
+    it('list', async () => {
+        const [{name,power}] = await context.read({name: MOCK_HERO_DEFAULT.name})
+        const result = {
+            name,
+            power
+        }
+        assert.deepStrictEqual(result, MOCK_HERO_DEFAULT)
+    }) 
+
+    it('update', async () => {
+        const result = await context.update(MOCK_HERO_ID, {
+            name: 'Marvin'
+        })
+
+        assert.deepStrictEqual(result.nModified, 1)
+    })
+
+    it('remove', async () => {
+        const result = await context.delete(MOCK_HERO_ID)
+        assert.deepStrictEqual(result.n, 1)
+    })
+})
