df0f0db45abb6f7fd515366db4261d6d2bde7c1d
eltoncampos1 <eltoncampos36@gmail.com>
2020-12-27 15:44:23 -0300

    set project  and use express-generator

---
+++ myapp/src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('hello')
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+})
