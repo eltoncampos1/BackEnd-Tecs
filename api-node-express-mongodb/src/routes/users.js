2e3811a250150d6a276f3b22856b564a7a8df40a
elnto de campos <eltoncampos36@gmail.com>
2021-01-03 20:06:53 -0300

    create users schema

--- api-node-express-mongodb/src/routes/users.js
+++ api-node-express-mongodb/src/routes/users.js
@@ -1,16 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (request, response) => {
-    return response.send({ message: "OK GET USERS"});
-});
+const Users = require('../models/user');
 
-router.post('/', (request, response) => {
-    return response.send({ message: "OK POST USERS"});
+router.get('/', (request, response) => {
+    Users.find({},(err, data) => {
+        if(err) {
+            return response.send({ error: "error in user consultation!"})
+        }
+
+        return response.send(data);
+    })
 });
 
 router.post('/create', (request,response) => {
-    return response.send({ message: "User created"})
-})
+    const { email, password } = request.body;
+
+    if (!email || !password) {
+        return response.send({ error: "insufficient data"})
+    }
+
+    Users.findOne({email}, (err, data) => {
+        if(err) {
+            return response.send({error: " Error to find user!"});
+        }
+
+        if (data) {
+            return response.send({ error: "User already registered"})
+        }
+
+        Users.create(request.body, (err,data) => {
+            if(err) {
+                return response.send({error: " Error to create user!"});
+            }
+
+            data.password = undefined;
+
+            return response.send(data);
+        });
+    });
+});
 
 module.exports = router;
