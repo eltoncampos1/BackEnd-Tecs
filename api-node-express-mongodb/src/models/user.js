084d20e408b36ec2f7d84ff94b221d562301b302
elnto de campos <eltoncampos36@gmail.com>
2021-01-03 20:48:16 -0300

    adding encryption to the password

--- api-node-express-mongodb/src/models/user.js
+++ api-node-express-mongodb/src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
     email: { 
@@ -17,6 +18,19 @@ const UserSchema = new Schema({
         type: Date,
         default: Date.now()
     }
+});
+
+UserSchema.pre('save', function(next) {
+    let user = this;
+    
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.hash(user.password, 10, (err, encrypted) => {
+        user.password = encrypted;
+        return next();
+    });
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- api-node-express-mongodb/src/routes/users.js
+++ api-node-express-mongodb/src/routes/users.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require('bcrypt');
+
+
 const Users = require('../models/user');
 
 router.get('/', (request, response) => {
     Users.find({},(err, data) => {
         if(err) {
-            return response.send({ error: "error in user consultation!"})
+            return response.send({ error: "error in user consultation!"});
         }
 
         return response.send(data);
@@ -17,7 +20,7 @@ router.post('/create', (request,response) => {
     const { email, password } = request.body;
 
     if (!email || !password) {
-        return response.send({ error: "insufficient data"})
+        return response.send({ error: "insufficient data"});
     }
 
     Users.findOne({email}, (err, data) => {
@@ -26,7 +29,7 @@ router.post('/create', (request,response) => {
         }
 
         if (data) {
-            return response.send({ error: "User already registered"})
+            return response.send({ error: "User already registered"});
         }
 
         Users.create(request.body, (err,data) => {
@@ -39,6 +42,34 @@ router.post('/create', (request,response) => {
             return response.send(data);
         });
     });
+});
+
+router.post('/auth', (request,response) => {
+    const {email, password} = request.body;
+
+    if(!email || !password) {
+        return response.send({ error: "insufficient data"});
+    }
+
+    Users.findOne({email}, (err, data) => {
+        if(err) {
+        return response.send({ error: "error to get user"});
+        }
+
+        if(!data) {
+            return response.send({ error: "unregistered user"});
+        }
+
+        bcrypt.compare(password, data.password, (err,same) => {
+            if(!same) {
+                return response.send({ error: "error to authenticating user"});
+            }
+
+            data.password = undefined;
+
+            return response.send(data);
+        });
+    }).select('+password');
 });
 
 module.exports = router;
